b4d4392b11798d905bbeb881cef327986b0d20cb
TuDV2000 <tudv2000@gmail.com>
2021-08-16 22:21:51 +0700

    init project

---
+++ AppDatVeXe/src/main/java/com/dvt/config/WebApplicationContextConfig.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dvt.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+/**
+ *
+ * @author VanTu
+ */
+@Configuration
+@EnableWebMvc
+@EnableTransactionManagement
+@ComponentScan(basePackages = {
+    "com.dvt.controllers",
+})
+public class WebApplicationContextConfig implements WebMvcConfigurer{
+    
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configuger) {
+        configuger.enable();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/**")
+                .addResourceLocations("/resources/css/");
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("/resources/images/");
+    }
+    
+    @Bean
+    public InternalResourceViewResolver getInternalResourceViewResolver(){
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        
+        resolver.setViewClass(JstlView.class);
+        resolver.setPrefix("/WEB-INF/jsp/");
+        resolver.setSuffix(".jsp");     
+        
+        return resolver;
+    }
+}
---
+++ AppDatVeXe/src/main/webapp/WEB-INF/jsp/index.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : index
+    Created on : Aug 16, 2021, 10:13:08 PM
+    Author     : VanTu
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Hệ thống đặt vé xe</title>
+    </head>
+    <body>
+        <h1>Hệ thống đặt vé xe</h1>
+    </body>
+</html>
