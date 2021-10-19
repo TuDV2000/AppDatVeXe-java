be39093e67d00f0c0f7644580fd0680dedfb4df1
TuDV2000 <tudv2000@gmail.com>
2021-10-19 23:38:29 +0700

    config HomeController

--- AppDatVeXe/src/main/java/com/dvt/controllers/HomeController.java
+++ AppDatVeXe/src/main/java/com/dvt/controllers/HomeController.java
@@ -5,22 +5,16 @@
  */
 package com.dvt.controllers;
 
-import com.dvt.pojos.User;
-import com.dvt.service.IPermissionService;
 import com.dvt.service.IPointService;
 import com.dvt.service.ITripService;
-import com.dvt.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpSession;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  *
@@ -45,16 +39,16 @@ public class HomeController {
     }
 
     @RequestMapping("/trips")
-    public String searchTrips(Model model
-            , @RequestParam(value = "sPoint", required = false) int sPointId
-            , @RequestParam(value = "ePoint", required = false) int ePointId
-            , @RequestParam(value = "sDate", required = false) String sDate){
-
-        System.out.println("controller: " + sDate);
-        System.out.println(tripService.getTrips(sPointId, ePointId, sDate));
-        if (sPointId != 0 && ePointId != 0 && sDate != null) {
-            model.addAttribute("trips", tripService.getTrips(sPointId, ePointId, sDate));
-        }
+    public String searchTrips(Model model){
+//            , @RequestParam(value = "sPoint", required = false) int sPointId
+//            , @RequestParam(value = "ePoint", required = false) int ePointId
+//            , @RequestParam(value = "sDate", required = false) String sDate){
+//
+//        System.out.println("controller: " + sDate);
+//        System.out.println(tripService.getTrips(sPointId, ePointId, sDate));
+//        if (sPointId != 0 && ePointId != 0 && sDate != null) {
+//            model.addAttribute("trips", tripService.getTrips(sPointId, ePointId, sDate));
+//        }
 
         return "schedule";
     }
